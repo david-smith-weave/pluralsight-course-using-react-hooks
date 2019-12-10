5096d9b8b68be5ab9552fe77f41a9fce302f3c9e
David Smith <david.smith@weaveability.com>
2019-12-10 13:16:58 +0000

    init input state

--- dps/pages/index.js
+++ dps/pages/index.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 
 const InputElement = () => {
+
+    const [inputText, setInputText] = useState("");
+
     return <input 
     onChange={(e) => {}}
     placeholder="Enter some text"/>;
