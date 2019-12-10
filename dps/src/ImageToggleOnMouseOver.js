8326a7b47de590ba051cd2839b4f3d1ddefebe04
David Smith <david.smith@weaveability.com>
2019-12-10 14:41:02 +0000

    add/remove scroll handlers using effect hook

--- dps/src/ImageToggleOnMouseOver.js
+++ dps/src/ImageToggleOnMouseOver.js
@@ -1,8 +1,15 @@
-import React, {useRef} from "react";
+import React, {useRef, useEffect} from "react";
 
 const ImageToggleOnMouseOver = ({ primaryImg, secondaryImg }) => {
 
     const imageRef = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler);
+        return ( () => {
+            window.removeEventListener("scroll", scrollHandler);
+        });
+    });
 
     return(
         <img 
