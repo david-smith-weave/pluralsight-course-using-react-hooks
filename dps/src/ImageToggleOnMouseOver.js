9dde066f939078ed3f64602763b3f0a878b1bc70
David Smith <david.smith@weaveability.com>
2019-12-10 14:41:54 +0000

    add in view state for images

--- dps/src/ImageToggleOnMouseOver.js
+++ dps/src/ImageToggleOnMouseOver.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useEffect, useState} from "react";
 
 const ImageToggleOnMouseOver = ({ primaryImg, secondaryImg }) => {
 
@@ -10,6 +10,8 @@ const ImageToggleOnMouseOver = ({ primaryImg, secondaryImg }) => {
             window.removeEventListener("scroll", scrollHandler);
         });
     });
+
+    const [inView, setInView] = useState(false);
 
     return(
         <img 
