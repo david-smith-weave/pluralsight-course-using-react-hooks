00ce0a61cc971ce8407193224fb213ad85c17f4d
David Smith <david.smith@weaveability.com>
2019-12-10 13:51:26 +0000

    replace image with toggle react component

--- dps/pages/ImageChangeOnMouseOver.js
+++ dps/pages/ImageChangeOnMouseOver.js
@@ -3,8 +3,14 @@ import React from "react";
 const ImageChangeOnMouseOver = () => {
     return (
         <div>
-            <img src="/static/speakers/bw/Speaker-187.jpg" alt="" />
-            <img src="/static/speakers/bw/Speaker-1124.jpg" alt="" />
+            <ImageToggleOnMouseOver 
+                primaryImg="/static/speakers/bw/Speaker-187.jpg" 
+                secondaryImg="/static/speakers/Speaker-187.jpg" 
+                alt="" />
+            <ImageToggleOnMouseOver 
+                primaryImg="/static/speakers/bw/Speaker-1124.jpg" 
+                secondaryImg="/static/speakers/Speaker-1124.jpg"
+                alt="" />
         </div>
     );
 };
