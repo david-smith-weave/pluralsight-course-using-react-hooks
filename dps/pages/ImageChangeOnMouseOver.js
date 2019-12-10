a1557e4363f7e9e1ea0120476a90dc48062a6bbc
David Smith <david.smith@weaveability.com>
2019-12-10 13:40:05 +0000

    image change on mouse over skeleton component

---
+++ dps/pages/ImageChangeOnMouseOver.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImageChangeOnMouseOver = () => {
+    return (
+        <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    );
+};
+
+export default ImageChangeOnMouseOver;
