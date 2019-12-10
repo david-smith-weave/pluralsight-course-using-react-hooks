3ae7ca059c7700f4143412c74ee72785e8d7a3c3
David Smith <david.smith@weaveability.com>
2019-12-10 13:54:17 +0000

    create image ref

--- dps/src/ImageToggleOnMouseOver.js
+++ dps/src/ImageToggleOnMouseOver.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 const ImageTogglerOnMouseOver = ({ primaryImg, secondaryImg }) => {
+
+    const imageRef = useRef(null);
+
     return(
         <img onMouseOver={() => {}} onMouseOut={() => {}}
             src={primaryImg}
