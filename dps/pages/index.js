0f34c6aa628aff5f72ac8ecafcc31082ee44be80
David Smith <david.smith@weaveability.com>
2019-12-10 13:15:54 +0000

    input change handler skeleton

--- dps/pages/index.js
+++ dps/pages/index.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 
 const InputElement = () => {
-    return <input placeholder="Enter some text"/>;
+    return <input 
+    onChange={(e) => {}}
+    placeholder="Enter some text"/>;
 }
 
 export default InputElement;
